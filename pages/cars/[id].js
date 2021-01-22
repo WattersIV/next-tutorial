903408cf4c6174d4b89f23da114d643edd3396d2
Bill Watters <billywatters13@gmail.com>
2021-01-22 14:46:16 -0500

    Using fallback: true to only pre render 1 page

--- pages/cars/[id].js
+++ pages/cars/[id].js
@@ -2,10 +2,17 @@ import { useRouter } from 'next/router'
 import Head from 'next/head'
 import styles from '../../styles/Home.module.css'
 
-export default function Car({ car }) {
-
+export default function Car(props) {
+  const { car } = props
+  console.log(props)
   const router = useRouter()
   const { id } = router.query
+
+  //If page is not pre rendered but exists this is returned while it loads
+  if (router.isFallback) {
+    return <div>Loading ...</div>
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -26,19 +33,21 @@ export default function Car({ car }) {
 
 export async function getStaticPaths() {
   const req = await fetch('http://localhost:3000/cars.json')
-  const data = await req.json() 
+  const data = await req.json()
 
-  const paths = data.map((car)=> {
-    return {
-      params: {
-        id: car
-      }
-    }
-  })
+  // const paths = data.map((car) => {
+  //   return {
+  //     params: {
+  //       id: car
+  //     }
+  //   }
+  // })
+
+  //console.log(paths)
 
   return {
-    paths, 
-    fallback: false
+    paths: [{params: {id: 'tesla'}}],
+    fallback: true
   }
 }
 
