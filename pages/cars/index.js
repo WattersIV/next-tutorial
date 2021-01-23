aea649eae7af14253f8333c2cb0c9fe7a466831c
Bill Watters <billywatters13@gmail.com>
2021-01-23 14:22:30 -0500

    Add material ui buttons with links to each car page

--- pages/cars/index.js
+++ pages/cars/index.js
@@ -1,3 +1,21 @@
+import { Button } from '@material-ui/core'
+
 export default function CarsList() {
-  return <h1>Cars List</h1>
+  return (
+    <>
+      <h1>Cars List</h1>
+      <li style={{display:'flex', flexDirection:'column', width: '40%'}}>
+      <Button variant='contained' color='primary' size='large' href='cars/tesla' style={{margin: '10px'}}> 
+        Tesla
+      </Button>
+      <Button variant='contained' color='primary' size='large' href='cars/lambo' style={{margin: '10px'}}> 
+        Lambo
+      </Button>
+      <Button variant='contained' color='primary' size='large' href='cars/ford' style={{margin: '10px'}}> 
+        Ford
+      </Button>
+      </li>
+    </>
+    )
+
 }
